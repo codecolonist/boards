5ca83b0789da06b21d35d51857d100bb0dbb444f
Ramkishorerao Gujjeti <ram6341@gmail.com>
2019-05-31 15:28:11 -0500

    rendering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BlackComponent } from './black/black.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlackComponent
   ],
   imports: [
     BrowserModule
